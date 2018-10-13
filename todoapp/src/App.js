c4e84b2ca09e8264e1a8f9731522effb6ee452e1
Erin Dubuc <erin.dubuc9@gmail.com>
2018-10-13 17:42:51 +0000

    set up Todos.js for todoApp

---
+++ todoapp/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Todos from './Todos'
+
+class App extends Component {
+  state = {
+          todos: [
+            {id: 1, content: 'buy some milk'},
+            {id: 2, content: 'play mario kart'}
+          ]
+  }
+  deleteTodo = (id) => {
+    // need to use filter which is not destructive
+    const todos = this.state.todos.filter(todo => {
+      // return true if keeping item in array
+      // return false if removing the item 
+      return todo.id !== id 
+      });
+      this.setState({
+        todos
+        })
+    }
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+      </div>
+    );
+  }
+}
+
+export default App;
