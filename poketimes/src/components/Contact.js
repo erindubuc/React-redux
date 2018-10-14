de8fcb2c9e8e7b08d1ed1ec620edb75e13703e2b
Erin Dubuc <erin.dubuc9@gmail.com>
2018-10-14 23:09:40 +0000

    set up react router in app.js and navbar.js

---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus vitae mi vitae iaculis. Praesent euismod varius condimentum. Suspendisse ultricies varius purus sed tristique. Ut laoreet mattis enim, at tempor diam cursus vitae. Sed vel turpis quis purus consequat auctor.</p>
+        </div>
+        )
+    }
+    
+    export default Contact
---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus vitae mi vitae iaculis. Praesent euismod varius condimentum. Suspendisse ultricies varius purus sed tristique. Ut laoreet mattis enim, at tempor diam cursus vitae. Sed vel turpis quis purus consequat auctor.</p>
+        </div>
+        )
+    }
+    
+    export default Home
