af6d32ce5142f0207d85372bb1f6a866bac14e01
Erin Dubuc <erin.dubuc9@gmail.com>
2018-10-14 23:30:09 +0000

    added Links/NavLinks to Navbar

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     return (
     <nav className="nav-wrapper red darken-3">
         <div className="container">
-            <a className="brand-logo" href="/">Poke'Times</a>
-            <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <a className="brand-logo" href="#top">Poke'Times</a>
+            <ul className="right"s>
+            <li><Link to="/">Home</Link></li>
+            <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
 
             </ul>
         </div>
