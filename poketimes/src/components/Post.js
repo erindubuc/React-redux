2899518e396456ac90c2be34be42036f252e856a
Erin Dubuc <erin.dubuc9@gmail.com>
2018-10-15 16:59:43 +0000

    added mapDispatchToProps within Post.js to delete posts

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class Post extends Component {
+    handleClick = () => {
+        this.props.deletePost(this.props.post.id);
+        this.props.history.push('/');
+        }
     render() {
         
         const post = this.props.post ? (
-            <div className="center">
-                <h4>{this.props.post.title}</h4>
+            <div className="post">
+                <h4 className="center">{this.props.post.title}</h4>
                 <p>{this.props.post.body}</p>
+                <div className="center">
+                <button className="btn grey" onClick={this.handleClick}>Delete Post</button>
+                </div>
             </div>
             ) : (
             <div className="center">Loading post...</div>
-            )
+            );
         
         return (
             <div className="container">
@@ -28,4 +35,12 @@ class Post extends Component {
             }
         }
     
-    export default connect(mapStateToProps)(Post)
+    const mapDispatchToProps = (dispatch) => {
+        return {
+            // function attached to props
+           deletePost: (id) => dispatch({type: 'DELETE_POST', id: id })  
+        }
+       }
+    
+   
+    export default connect(mapStateToProps, mapDispatchToProps)(Post)
